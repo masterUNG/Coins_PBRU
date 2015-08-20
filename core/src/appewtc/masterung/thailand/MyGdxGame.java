e7ebf007d9f6bca4e40ce0894930dbf23757a251
masterUNG <phrombutr@gmail.com>
2015-08-20 15:03:58 +0700

    BitMapFont

--- core/src/appewtc/masterung/thailand/MyGdxGame.java
+++ core/src/appewtc/masterung/thailand/MyGdxGame.java
@@ -2,26 +2,61 @@ package appewtc.masterung.thailand;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	//Explicit
+	private SpriteBatch batch;
+	private Texture wallpaperTexture;
+	private OrthographicCamera objOrthographicCamera;
+	private BitmapFont nameBitmapFont;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+
+		//คือการกำหนดขนาดของจอที่ต้องการ
+		objOrthographicCamera = new OrthographicCamera();
+		objOrthographicCamera.setToOrtho(false, 1200, 800);
+
+		//SetUP WallPaper
+		wallpaperTexture = new Texture("background.png");
+
+		//SetUP BitMapFont
+		nameBitmapFont = new BitmapFont();
+		nameBitmapFont.setColor(Color.RED);
+		nameBitmapFont.setScale(4);
+
+	}	// create เอาไว้กำหนดค่า
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//Setup Screen
+		objOrthographicCamera.update();
+		batch.setProjectionMatrix(objOrthographicCamera.combined);
+
+
+
+		//เอาไว้วาด Object
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		//Drawable Wallpaper
+		batch.draw(wallpaperTexture, 0, 0);
+
+		//Drawable BitMapFont
+		nameBitmapFont.draw(batch, "Coins PBRU", 50, 750);
+
+
 		batch.end();
-	}
-}
+
+	}	// render ตัวนี่คือ loop
+}	// Main Class
